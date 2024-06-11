f3d81c623c3ae07c9b289d498a0afdb77f020d13
hemy08 <zhaojunwei008@yeah.net>
2024-06-11 17:54:04 +0800

    【Title】: material 增加admonition卡片实时渲染 【DTS&Issues&E2E】: 【Description】: 【Root Cause】: 【Solution】: 【Impact Scope】: 【Interface】: 【Document】: 【Self Test Report】:

--- src/main/utils/MaterialRender.ts
+++ src/main/utils/MaterialRender.ts
@@ -1,48 +1,33 @@
-function materialAdmonitionsParserContent(text: string): string {
-  // 因为匹配的字符串，已经去掉了"!!! note "前缀，直接进行分割即可
-  // 使用第一个换行符来分割字符串，获取 "!!! note " 后面的内容
-  console.log('text', text)
-  const content = text.split('\n')
-  console.log('content', content)
-  // 如果 remainingText 以换行符开始，则去掉它
-  /*if (content.startsWith('\n')) {
-    content = content.substring(1)
-  }*/
-
-  return content.slice(1).join('\n')
-}
-
-function materialAdmonitionsParserTitle(text: string): string {
-  // 因为匹配的字符串，已经去掉了"!!! note "前缀，直接进行分割即可
-  // 使用 split 分割字符串，找到第一个和第二个双引号之间的内容
-  const parts = text.split('"')
-  if (parts.length >= 3) {
-    return parts[1]
+function materialParserAdmonitionsContent(text: string): {
+  type: string
+  title: string
+  content: string
+} {
+  return {
+    type: 'note',
+    title: 'string',
+    content: text
   }
-  return ''
 }
 
 export function materialAdmonitions(text: string): string {
   let renderResult = text
   let match: RegExpExecArray | null = null
-  const regex = /^!!! note ([\s\S]*?)\n\s*\n$/g
+  // 匹配字符串中所有以!!!开始的内容（直到遇到另一个!!!、#或者字符串的结尾）
+  const regex = /!!!([\s\S]*?)(?=!!!|#|$)/g
   // 使用全局搜索来查找所有匹配项，匹配到的字符串，已经去掉了前缀和后缀
-  while ((match = regex.exec(text)) !== null) {
-    const title = materialAdmonitionsParserTitle(match[1])
-    console.log('title', title)
-    const content = materialAdmonitionsParserContent(match[1])
-    console.log('content', content)
+  while ((match = regex.exec(renderResult)) !== null) {
+    console.log('title', match[1])
+    const content = materialParserAdmonitionsContent(match[1])
+    //console.log('title', title)
+    //const content = materialAdmonitionsParserContent(match[1])
+    //console.log('content', content)*/
     const renderHtml =
-      '<div class="admonition note" style="border-color: #448aff"><p class="admonition-title">"' +
-      title +
-      '"</p><p>"' +
-      content +
-      '"</p> </div>' +
-      '\n\n'
+      `<div class="admonition ${content.content}">` +
+      `<p class="admonition-title">${content.title}</p>` +
+      `<p>${content.content}</p></div><br><br>`
     renderResult = renderResult.replace(match[0], renderHtml)
   }
-
-  console.log('renderResult', renderResult)
 
   return renderResult
 }
