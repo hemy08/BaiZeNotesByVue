c4cf5e33710514e39631aead7b2e51c748ac77f6
hemy08 <zhaojunwei008@yeah.net>
2024-07-09 09:16:22 +0800

    【Title】: 增加编译拷贝动作 【DTS&Issues&E2E】: 【Description】: 【Root Cause】: 【Solution】: 【Impact Scope】: 【Interface】: 【Document】: 【Self Test Report】:

--- copyfile.bat
+++ copyfile.bat
@@ -1,3 +1,5 @@
 rmdir /S /Q /Y "out\renderer\src\lib"
 xcopy /S /E /Y "src\renderer\src\lib" "out\renderer\src\lib"
 
+rmdir /S /Q /Y "out\renderer\src\dialogs"
+xcopy /S /E /Y "src\renderer\src\dialogs" "out\renderer\src\dialogs"
